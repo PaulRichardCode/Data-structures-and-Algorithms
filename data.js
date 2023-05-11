34da33503f2a6a8a04aabe26e2e310b5434e0ef0
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-11 06:40:04 +0100

    ok

--- data.js
+++ data.js
@@ -1,1 +1,48 @@
-console.log("hello");
+/* let p = new Promise((resolve, reject) => {
+  let a = 1 + 4;
+  if (a == 2) {
+    resolve("accepted");
+  } else {
+    reject("failed");
+  }
+});
+
+//catch is executed when promise is rejected
+//then is executed when promise is accepted
+
+p.then((message) => {
+  console.log("This is in the then" + message);
+}).catch((message) => {
+  console.log("This is in the catch" + message);
+});
+ */
+//****************
+//****************
+//FETCH
+
+/* fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "User 1",
+  }),
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data));
+   */
+
+fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "Paul RICHARD",
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data));
