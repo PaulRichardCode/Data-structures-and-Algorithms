f917b115c613a806a4f656ffcd41796745f198fc
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-17 13:01:09 +0100

    ok

--- data.js
+++ data.js
@@ -35,14 +35,14 @@ p.then((message) => {
   .then((data) => console.log(data));..
    */
 
-fetch("https://reqres.in/api/users", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    name: "Paul RICHARD",
-  }),
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://reqres.in/api/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Paul RICHARD",
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
